39a2abb58021e57596c290b90da9b36d9e4059df
lizagrigarchuk <liza-grig@mail.ru>
2019-12-04 09:05:30 +0300

    newCommit

--- js/script.js
+++ js/script.js
@@ -1,85 +1,37 @@
 
 window.onload = function () {
-  /*  let set = new Set();
-    console.log(set);
+var cache = [];
 
-    function multiAB(a,b){
-        var multi = a*b;
-        if(set.has(`${b}*${a}=${multi}`)){
-            console.log('Такое значение уже есть');
-            return multi;
-        }
-        else if(set.size==3){ //?? удаления первого
-          var k = 0;
-          set.forEach(
-            user => {
-              if(k!=1){
-                var temp = user;
-                k++;
-                set.delete(temp);
-              }
-            } );
-              console.log("Удаляем ");
-              console.log(set);
-            var str = `${a}*${b}=${multi}`
-            set.add(`${a}*${b}=${multi}`);
-            return multi;
-        }
-        else {
-          console.log("Добавляем");
-          console.log(set);
-          console.log(set);
-          set.add(`${a}*${b}=${multi}`);
-          return multi;
-        }
-    }
-    multiAB(2,5);
-    multiAB(3,7);
-    multiAB(5,2);
-    multiAB(-2,5);
-    multiAB(56,5);
-*/
-
-let array = [];
-var sizeArray = 0;
-
-  function multiAB(a,b){
-    var flag = 0;
-
-    for (var i = 0; i < array.length; i++) {
-      if(array[i].a==a && array[i].b==b || array[i].a==b && array[i].b==a){
-        flag=1;
-          return `Такие числа уже были, произведение = ${array[i].mult}`;
-      }
-    }
-
-    if(flag!=1){
-      if(array.length<3){
-        array[sizeArray]={
+  function multiAB(){
+    return function (a,b){
+      let cached = false;
+      cache.forEach((val,i)=>{
+        (val.a==a && val.b==b) || (val.a==b && val.b==a) ? cached=`Такие числа уже были: ${cache[i].mult}`:null
+      })
+      if(cached) return cached;
+      if(cache.length!=3){
+        cache.push({
           a:a,
           b:b,
-          mult:a*b
-        };
-        sizeArray++;
-        return a*b;
+          mult: a*b
+        })
       }
-      else if(array.length==3){
-          array.shift();
-          array[2]={
-            a:a,
-            b:b,
-            mult:a*b
-          };
-            return a*b;
+      else {
+        cache.push({
+          a:a,
+          b:b,
+          mult: a*b
+        });
+        cache.shift();
       }
+      return `Новый результат: ${cache[cache.length-1].mult}`;
     }
   }
-
-  console.log(multiAB(2,5));
-  console.log(multiAB(3,7));
-  console.log(multiAB(2,5));
-  console.log(multiAB(5,2));
-  console.log(multiAB(-2,5));
-  console.log(multiAB(56,5));
-  console.log(multiAB(24,2));
+  console.log(multiAB()(2,5));
+  console.log(multiAB()(3,7));
+  console.log(multiAB()(2,5));
+  console.log(multiAB()(5,2));
+  console.log(multiAB()(-2,5));
+  console.log(multiAB()(56,5));
+  console.log(multiAB()(24,2));
 }
